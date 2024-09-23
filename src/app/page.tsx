301e86576b981ab7aa740799112c4fe5f9736ebc
Chaolla Park <chaollapark@gmail.com>
2024-09-23 17:41:32 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import Hero from "@/app/components/Hero";
+import Jobs from "@/app/components/Jobs";
+import { addOrgAndUserData, JobModel } from "@/models/Job";
+import { getUser } from "@workos-inc/authkit-nextjs";
+
+// Database connection
+const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  return mongoose.connect(process.env.MONGO_URI as string, {
+    serverSelectionTimeoutMS: 5000, // Increase this value if necessary
+    socketTimeoutMS: 45000, // Increase this value if necessary
+  });
+};
+
+export default async function Home() {
+  await connectDB();  // Ensure connection is established
+  const { user } = await getUser();
+  const latestJobs = await addOrgAndUserData(
+    await JobModel.find({}, {}, { limit: 5, sort: '-createdAt' }),
+    user,
+  );
+  return (
+    <>
+      <Hero />
+      <Jobs header={''} jobs={latestJobs} />
+    </>
+  );
+}
---
+++ types/react-country-state-city.d.ts
@@ -0,0 +1,3 @@
+declare module 'react-country-state-city';
+declare module 'react-timeago';
+  
