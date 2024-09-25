c92ea399ac392d6fd182b3a73dfe74cdb343d2a5
Chaolla Park <chaollapark@gmail.com>
2024-09-25 03:26:38 +0200

    fixes to the database stringent requirments, also error handling for models job

--- src/models/Job.ts
+++ src/models/Job.ts
@@ -27,43 +27,44 @@ export type Job = {
 };
 
 const JobSchema = new Schema({
-  title: {type: String, required: true},
-  description: {type: String, required: true},
-  remote: {type: String, required: true},
-  type: {type: String, required: true},
-  salary: {type: Number, required: true},
-  country: {type: String, required: true},
-  state: {type: String, required: true},
-  city: {type: String, required: true},
-  countryId: {type: String, required: true},
-  stateId: {type: String, required: true},
-  cityId: {type: String, required: true},
+  title: {type: String },
+  description: {type: String },
+  type: {type: String },
+  salary: {type: Number },
+  country: {type: String },
+  state: {type: String },
+  city: {type: String },
+  countryId: {type: String },
+  stateId: {type: String },
+  cityId: {type: String },
   jobIcon: {type: String},
   contactPhoto: {type: String},
-  contactName: {type: String, required: true},
-  contactPhone: {type: String, required: true},
-  contactEmail: {type: String, required: true},
-  orgId: {type: String, required: true},
+  contactName: {type: String },
+  contactPhone: {type: String },
+  contactEmail: {type: String },
+  orgId: {type: String },
 }, {
   timestamps: true,
 });
 
 export async function addOrgAndUserData(jobsDocs:Job[], user:User|null) {
   jobsDocs = JSON.parse(JSON.stringify(jobsDocs));
-  await mongoose.connect(process.env.MONGO_URI as string);
-  const workos = new WorkOS(process.env.WORKOS_API_KEY);
-  let oms:AutoPaginatable<OrganizationMembership>|null = null;
-  if (user) {
-    oms = await workos.userManagement.listOrganizationMemberships({
-      userId: user?.id,
-    });
-  }
-  for (const job of jobsDocs) {
-    const org = await workos.organizations.getOrganization(job.orgId);
-    job.orgName = org.name;
-    if (oms && oms.data.length > 0) {
-      job.isAdmin = !!oms.data.find(om => om.organizationId === job.orgId);
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string);
+    const workos = new WorkOS(process.env.WORKOS_API_KEY);
+    let oms: AutoPaginatable<OrganizationMembership> | null = null;
+    if (user) {
+      oms = await workos.userManagement.listOrganizationMemberships({ userId: user?.id });
     }
+    for (const job of jobsDocs) {
+      const org = await workos.organizations.getOrganization(job.orgId);
+      job.orgName = org.name;
+      if (oms && oms.data.length > 0) {
+        job.isAdmin = !!oms.data.find(om => om.organizationId === job.orgId);
+      }
+    }
+  } catch (error) {
+    console.error('Error in addOrgAndUserData:', error);
   }
   return jobsDocs;
 }
