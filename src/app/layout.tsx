97a2dad2e1e77bd136540c2e6abaa57fbbe4c85e
Chaolla Park <chaollapark@gmail.com>
2024-11-13 06:30:31 +0100

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import '@radix-ui/themes/styles.css';
 import Link from 'next/link';
+import GoogleAnalytics from "@/app/components/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -37,6 +38,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <GoogleAnalytics />
         <Header />
         {children}
         <footer className="container py-8 text-gray-500">
